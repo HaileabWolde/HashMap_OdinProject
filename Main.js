d46f3f467b1d8312008c7d356fe7af1980fbfa62
Haileab <womahaileab1428@gmail.com>
2025-08-20 12:54:47 +0300

    creating hashing functions

--- Main.js
+++ Main.js
@@ -1,7 +1,21 @@
 class HashMap {
-    constructor(initialCapacity = 10, loadFactorThreshold = 0.7) {
+    constructor(initialCapacity = 16, loadFactorThreshold = 0.7) {
         this.loadFactorThreshold = loadFactorThreshold;
         this.capacity = initialCapacity;
         this.bucket = new Array(this.capacity).fill(null).map(() => []);
+    }
+    hash(key){
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++){
+            hashCode = (hashCode * primeNumber + key.charCodeAt(i)) % this.capacity;
+        }
+        return hashCode;
+    }
+    set (key, value){
+        const index = this.hash(key);
+        this.bucket[index] = this.bucket[index] || [];
+        this.bucket[index].push([key, value]);
     }
 }
