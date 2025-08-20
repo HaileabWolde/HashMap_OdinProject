a749fc11a6f14bd1d7e91f38e7fd56657022425e
Haileab <womahaileab1428@gmail.com>
2025-08-20 12:43:20 +0300

    creating the hashmap function

--- Main.js
+++ Main.js
@@ -1,1 +1,7 @@
-console.log("Hell`o, World!");
+class HashMap {
+    constructor(initialCapacity = 10, loadFactorThreshold = 0.7) {
+        this.loadFactorThreshold = loadFactorThreshold;
+        this.capacity = initialCapacity;
+        this.bucket = new Array(this.capacity).fill(null).map(() => []);
+    }
+}
